2721812bb8af0031af8d5c6a76b15b39068f94a4
GabrielCrackPro <gabrielcrackpro2001@gmail.com>
2022-03-10 14:35:47 +0100

    Dragons page added 💻

--- public/app.js
+++ public/app.js
@@ -3,6 +3,7 @@ import functions from "./services/functions.js";
 const companyInfoContainer = document.querySelector(".company-info");
 const capsulesContainer = document.querySelector(".capsule-info");
 const crewContainer = document.querySelector(".crew-info");
+const dragonsContainer = document.querySelector(".dragon-info");
 
 window.onload = async () => {
   const url = window.location.href;
@@ -12,5 +13,7 @@ window.onload = async () => {
     await functions.getCapsules(capsulesContainer);
   } else if (url.endsWith("crew.html")) {
     await functions.getCrew(crewContainer);
+  } else if (url.endsWith("dragons.html")) {
+    await functions.getDragons(dragonsContainer);
   }
 };
--- public/services/functions.js
+++ public/services/functions.js
@@ -118,5 +118,78 @@ const functions = {
       `;
     });
   },
+  getDragons: async (element) => {
+    const dragonsInfo = await functions.getData(links.dragons);
+    console.log(dragonsInfo);
+    dragonsInfo.forEach((dragon) => {
+      element.innerHTML += `
+      <div class="card mb-3">
+      <div class="row no-gutters">
+        <div class="col-md-12">
+          <div class="card-header bg-primary">
+            <h5 class="card-title text-center text-white">${dragon.name}</h5>
+          </div>
+          <div class="card-body">
+            <p class="card-text"><span class="bg-primary text-white">Status: </span>${
+              dragon.active ? "Active" : "Disabled"
+            }</p>
+            <p class="card-text"><span Capacity: class="bg-primary text-white">Capacity:</span> ${
+              dragon.crew_capacity
+            }</p>
+            <p class="card-text"><span class="bg-primary text-white">Diameter: </span> ${
+              dragon.diameter.meters
+            }m</p>
+            <p class="card-text"><span class="bg-primary text-white">Dry Mass: </span> ${
+              dragon.dry_mass_kg
+            } kg</p>
+            <p class="card-text"><span class="bg-primary text-white">First Flight: </span>${
+              dragon.first_flight
+            }</p>
+            <p class="card-text"><span class="bg-primary text-white">Orbit Duration: </span>${
+              dragon.orbit_duration_yr
+            } years</p>
+            <p class="card-text bg-primary text-white p-2">Heat Shield:</p>
+            <ul class="list-group list-group-flush">
+              <li class="list-group-item"><span class="bg-primary text-white">Material: </span>${
+                dragon.heat_shield.material
+              }</li>
+              <li class="list-group-item"><span class="bg-primary text-white">Size: </span>${
+                dragon.heat_shield.size_meters
+              } m</li>
+              <li class="list-group-item"><span class="bg-primary text-white">Temp: </span>${
+                dragon.heat_shield.temp_degrees
+              } °C</li>
+            </ul>
+            <p class="card-text bg-primary text-white p-2">First Flight Payload:</p>
+            <ul class="list-group list-group-flush">
+              <li class="list-group-item"><span class="bg-primary text-white">Cargo: </span>${Object.keys(
+                dragon.trunk.cargo
+              )[0]
+                .split("_")
+                .join(" ")} - ${dragon.trunk.cargo.solar_array}</li>
+                
+              <li class="list-group-item"><span class="bg-primary text-white">Mass: </span>${
+                dragon.launch_payload_mass.kg
+              } kg</li>
+              <li class="list-group-item"><span class="bg-primary text-white">Volume: </span>${
+                dragon.launch_payload_vol.cubic_meters
+              } m3</li>
+              <li class="list-group-item"><span class="bg-primary text-white">Return Mass: </span>${
+                dragon.return_payload_mass.kg
+              } kg</li>
+              <li class="list-group-item"><span class="bg-primary text-white">Return Volume: </span>${
+                dragon.return_payload_vol.cubic_meters
+              } m3</li>
+            </ul>
+            <p class="card-text"><span class="bg-primary text-white">Description: </span>${
+              dragon.description
+            }</p>
+          </div>
+        </div>
+      </div>
+    </div>
+      `;
+    });
+  },
 };
 export default functions;
